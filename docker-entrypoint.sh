53f74b0123b09a836c6d528e1c51f680fc2e533c
Brandon Wallace <brandonmwallace97@gmail.com>
2026-06-02 10:38:35 -0700

    fix?

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 set -e
 
+echo "Waiting for database..."
+until node -e "
+const { Client } = require('pg');
+const c = new Client({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT) || 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+});
+c.connect().then(() => c.end()).then(() => process.exit(0)).catch(() => process.exit(1));
+" >/dev/null 2>&1; do
+  sleep 2
+done
+
 echo "Running database migrations..."
 npm run migrate
 
